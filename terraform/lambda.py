73311b4a8f3bd165052aa57f074fe54a4f0d7f4a
Brian Lin <briancclin@gmail.com>
2023-05-07 14:19:38 -0400

    style: format python with black

--- terraform/lambda.py
+++ terraform/lambda.py
@@ -1,26 +1,23 @@
 import json
 import boto3
 
+
 def lambda_handler(event, context):
-    dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table('VisitorCount')
+    dynamodb = boto3.resource("dynamodb")
+    table = dynamodb.Table("VisitorCount")
 
     res = table.update_item(
-        Key={
-            'Site': "Resume"
-        },
-        
+        Key={"Site": "Resume"},
         UpdateExpression="SET VisitorCount = if_not_exists(VisitorCount, :start) + :inc",
-
         ExpressionAttributeValues={
-            ':inc': 1,
-            ':start': 0,
+            ":inc": 1,
+            ":start": 0,
         },
-        ReturnValues="UPDATED_NEW"
+        ReturnValues="UPDATED_NEW",
     )
-    visitor_count = res['Attributes']['VisitorCount']
-    response_code = res['ResponseMetadata']['HTTPStatusCode']
+    visitor_count = res["Attributes"]["VisitorCount"]
+    response_code = res["ResponseMetadata"]["HTTPStatusCode"]
     return {
-        'statusCode': response_code,
-        'body': json.dumps(f"Number of visitors: {visitor_count}")
+        "statusCode": response_code,
+        "body": json.dumps(f"Number of visitors: {visitor_count}"),
     }
