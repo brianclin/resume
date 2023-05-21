e661bd7fd097e80af42064bb207dd1edfa65a414
Brian Lin <briancclin@gmail.com>
2023-05-20 22:16:32 -0400

    Revert "style: add prettier to precommit"
    This reverts commit f30ca60e57f45736baee449d9dcdda6bc908246d.

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,13 @@
-import { generate } from "critical";
-import gulp from "gulp";
+import {generate} from 'critical';
+import gulp from 'gulp';
 
-gulp.task("critical", () => {
-  return generate({
-    inline: true,
-    base: "terraform/",
-    src: "index.html",
-    target: "index-critical.html",
-    width: 1300,
-    height: 900,
-  });
+gulp.task('critical', () => {
+    return generate({
+        inline: true,
+        base: 'terraform/',
+        src: 'index.html',
+        target: 'index-critical.html',
+        width: 1300,
+        height: 900,
+      });
 });
