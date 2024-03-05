c80c3d90f6832c21e09bb3a8558bd7fbcd052235
Brian Lin <briancclin@gmail.com>
2024-03-05 08:51:05 -0500

    feat: remove hosted zone

--- main.tf
+++ main.tf
@@ -1,10 +1,9 @@
 module "resume" {
   source = "git::https://github.com/brianclin/terraform-aws-s3-website.git?ref=v1.2.0"
 
-  domain_name            = "brianclin.dev"
-  bucket_name            = "blin-resume"
-  create_hosted_zone     = true
-  enable_count_api       = true
-  api_gateway_invoke_url = module.api_gateway.default_apigatewayv2_stage_invoke_url
-  index_html_source      = "index-critical.html"
+  domain_name        = "brianclin.dev"
+  bucket_name        = "blin-resume"
+  create_hosted_zone = false
+  enable_count_api   = false
+  index_html_source  = "index-critical.html"
 }
